25bb696e3a2f9a31158fa788f6b14f8ad19901f4
Zaw Ye Htet (DAT) <zawyehtet7928@gmail.com>
2024-10-24 13:59:30 +0630

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+require("express-ws")(app);
+const prisma = require("./prismaClient");
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const { userRouter } = require("./routers/user");
+app.use("/", userRouter);
+
+const { contentRouter } = require("./routers/content");
+app.use("/content", contentRouter);
+
+const { wsRouter } = require("./routers/ws");
+app.use("/", wsRouter);
+
+// for test
+app.get("/info", (req, res) => {
+    res.json({ msg: "Sos API" });
+});
+
+app.listen(8000, () => {
+    console.log("SOS 52 API started at 8000");
+});
+
+
+const gracefulShutdown = async () => {
+    await prisma.$disconnect();
+    server.close(() => {
+        console.log("Yaycha API closed.");
+        process.exit(0);
+    });
+};
+
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);
---
+++ prisma/seeds/main.js
@@ -0,0 +1,25 @@
+const { PrismaClient } = require("@prisma/client");
+
+const {LikeSeeder} = require("./LikeSeeder");
+
+const prisma = new PrismaClient();
+
+const { UserSeeder } = require("./UserSeeder");
+const { PostSeeder } = require("./PostSeeder");
+const { CommentSeeder } = require("./CommentSeeder");
+
+async function main() {
+    try {
+        await UserSeeder();
+        await PostSeeder();
+        await CommentSeeder();
+        await LikeSeeder();
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+main();
